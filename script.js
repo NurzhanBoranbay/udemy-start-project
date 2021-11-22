e00b9f5554ba75f7d121345d068e539d1dc7ff71
NurzhanBoranbay <boranbaynurzhan@gmail.com>
2021-11-22 12:34:03 +0600

    add genres

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посматрели?', '');
+
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = prompt('Сколько фильмов вы уже посматрели?', '');
+
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = prompt('Сколько фильмов вы уже посматрели?', '');
+   }
+}
+
+start();
 
 const personalMovieDB = {
    count: numberOfFilms,
@@ -8,12 +19,48 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+function detectedPersonalLevel() {
+   if (personalMovieDB.count < 10) {
+      alert('Просмотрено довольно мало фильмов!');
+   } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+      alert('Вы классически зритель!');
+   } else if (personalMovieDB.count >= 30) {
+      alert('Вы киноман!');
+   } else {
+      alert('Пройзошло ошибка!');
+   }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+detectedPersonalLevel();
 
-console.log(personalMovieDB);
+function rememberMyFilms() {
+   for (let i = 0; i < 2; i++) {
+      let a = prompt('Один из просмотренных фильмов?', '');
+      let b = prompt('На сколько оцените его?', '');
+   
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+      } else {
+         i--;
+      }
+   }
+}
+
+rememberMyFilms();
+
+function showMyDB(hidden) {
+   if (!hidden) {
+      console.log(personalMovieDB);
+   }
+}
+
+function writeYourGenres(genres) {
+   for(let i = 1; i < 4; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+      genres[i - 1] = genre;
+   }
+   //console.log(genres);
+}
+
+showMyDB(personalMovieDB.privat);
+writeYourGenres(personalMovieDB.genres);
